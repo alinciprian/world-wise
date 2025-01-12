fa3dca8ec39211d088fe1c013b1c205055079178
Ciprian Eniță <ciprian_enita@yahoo.com>
2025-01-12 13:52:58 +0200

    small corrrections

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,9 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />} >
-          <Route path='cities' element={<p>List of cities</p>}/>
-          <Route path='countries' element={<p>Countries</p>}/>
-          <Route path='form' element={<p>Form</p>}/>
+          <Route path='cities' element={<p>List of some cities</p>}/>
+          <Route path='countries' element={<p>COUNTRIES</p>}/>
+          <Route path='form' element={<p>This is the form</p>}/>
         </Route>
         <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
