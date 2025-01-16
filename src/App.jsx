20e1b3043b4d7491c19f25ff25e93fbd4386228e
alinciprian <ciprian_enita@yahoo.com>
2025-01-16 15:24:52 +0200

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import CityList from './components/CityList';
 import { useEffect, useState } from 'react';
 import CountryList from './components/CountryList';
 import City from './components/City';
+import Form from './components/Form';
 
 const BASE_URL = 'http://localhost:9000';
 
@@ -54,7 +55,7 @@ function App() {
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
 
-          <Route path="form" element={<p>This is the form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
